5e0f5ba3a6f65552ac78663191de806b6a06a8c6
Saricc <saramunitic94@gmail.com>
2023-06-25 21:30:56 +0200

    test

--- src/components/Kupi.js
+++ src/components/Kupi.js
@@ -3,7 +3,7 @@ export default function Kupi() {
     return (
         <div>
             <div style={{ fontSize: 20, color: "pink", display: "flex", justifyContent: "center" }}>o nama</div>
-            <div>o vejvu</div>
+            <div>o vejvu ddddd</div>
             <div>o vejvu</div>
 
             <div>o vejvu</div>
